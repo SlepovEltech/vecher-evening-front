bfb56006f4cd24a2b014b623bf601a0f8ee8070f
Artem Slepov <artemslepov01@gmail.com>
2021-05-02 03:21:06 +0300

    deploy

---
+++ precache-manifest.16b176da22258d520f97b1828c34be5b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246a0c738709ad6501da",
+    "url": "/vecher-evening-front/css/app.a83511ad.css"
+  },
+  {
+    "revision": "1f0a0db2497d71ba6073",
+    "url": "/vecher-evening-front/css/chunk-14ba5008.24830379.css"
+  },
+  {
+    "revision": "63b272442f7c28d8f720",
+    "url": "/vecher-evening-front/css/chunk-4355cc20.769b2bec.css"
+  },
+  {
+    "revision": "e82fc740365711754a7a",
+    "url": "/vecher-evening-front/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "eaf92a2cfce74e3b0eee6444fa4646e8",
+    "url": "/vecher-evening-front/img/bg.eaf92a2c.jpeg"
+  },
+  {
+    "revision": "a7437b2d3b51f949fb01598c31439870",
+    "url": "/vecher-evening-front/index.html"
+  },
+  {
+    "revision": "246a0c738709ad6501da",
+    "url": "/vecher-evening-front/js/app.21ed795e.js"
+  },
+  {
+    "revision": "1f0a0db2497d71ba6073",
+    "url": "/vecher-evening-front/js/chunk-14ba5008.23019b1a.js"
+  },
+  {
+    "revision": "b38826277e00d89278e1",
+    "url": "/vecher-evening-front/js/chunk-2d0b21b6.fd3c0c91.js"
+  },
+  {
+    "revision": "63b272442f7c28d8f720",
+    "url": "/vecher-evening-front/js/chunk-4355cc20.dc8dded9.js"
+  },
+  {
+    "revision": "9a8829e019932776a781",
+    "url": "/vecher-evening-front/js/chunk-d772d1a8.1f4d55cc.js"
+  },
+  {
+    "revision": "e82fc740365711754a7a",
+    "url": "/vecher-evening-front/js/chunk-vendors.bc9c020c.js"
+  },
+  {
+    "revision": "ebd46fafda237c2a896a617e943a634e",
+    "url": "/vecher-evening-front/logo.jpeg"
+  },
+  {
+    "revision": "7fc389fb38243fd88a2d896f0e66357f",
+    "url": "/vecher-evening-front/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vecher-evening-front/robots.txt"
+  }
+]);
